b118c6df4eb22ba280eaff0568734b337e9f7fd7
McKenshel Cameau <mcameau20@gmail.com>
2025-07-23 00:19:06 -0400

    made initial project structure

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-async def root():
-    return {
-    
-    }
+def main(query:str):
+
+    if "spicy" or "hamburgers" in query.lower:
+        return {'food':["chipotle","burger king","whole foods"]}
+    else:
+        return {'food':["Wendys","99 Restaurant","Legal Seafoods"]}
+

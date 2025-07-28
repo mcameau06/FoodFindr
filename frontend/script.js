e775031ec331a94e4d43baf826f19591d1ec858e
McKenshel Cameau <mcameau20@gmail.com>
2025-07-28 08:43:30 -0400

    got frontend to work

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,9 @@
+
+
+async function searchFood() {
+const query =  document.getElementById("foodQuery").value;
+const lat = 42.18383
+const lng = -71.03
 const url = `http://127.0.0.1:8000/search?query=${query}&lat=${lat}&lng=${lng}`;
 
 
@@ -5,23 +11,31 @@ try{
   const response = await fetch(url);
   const data = await response.json();
 
-  const listOfResults = document.getElementById("result")
+  const listOfResults = document.getElementById("results")
   listOfResults.innerHTML = "";
-
-  data.array.forEach(place => {
+  if (data && data.results && Array.isArray(data.results) && data.results.length > 0)
+  {data.forEach(place => {
     const li = document.createElement("li");
     li.textContent = `${place.name} - ${place.address}`;
-    listOfResults.appendChild(li)
+    listOfResults.appendChild(li);
   });
+}
+else{
+  const li = document.createElement("li");
+  li.textContent = "No results Found";
+  listOfResults.appendChild(li)
+}
 }
 catch(error)
 {
+  const listOfResults = document.getElementById("results");
+  listOfResults.innerHTML = "<li>error loading results</li>"
   console.error("Error fetching food ",error)
 
 }
 
+}
 
 
 const query = "soup"
-const lat = 42.18383
-const lng = -71.03
+

5bf9614b139b9337b9212aebfe5b70ea76a66dec
McKenshel Cameau <mcameau20@gmail.com>
2025-08-20 04:22:54 -0400

    added main.py

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+app = FastAPI()
+
+load_dotenv()
+origins = list(os.getenv("ORIGINS").split(","))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+import routes
